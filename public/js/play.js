fd99c0bd090c359b1bb35187a170915a7ceb593a
Alexander Hartigan <alexandermhartigan@gmail.com>
2025-03-01 21:34:15 -0800

    added win checker

--- public/js/play.js
+++ public/js/play.js
@@ -3,8 +3,35 @@ const turnDisplay = document.getElementById("turn-display")
 
 let turn = true;
 
+// Check for game winner
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+const board =  [null, null, null, null, null, null, null, null, null];
+
+function checkWinner() {
+    for ([a, b, c] of winningCombinations) {
+        if (board[a] && board[a] == board[b] && board[a] == board[c]) {
+            console.log(`Winner is ${board[a]}!`);
+            return;
+        }
+    }
+    return;
+}
+
+// Add text to boxes
 function addLetter(box) {
     const boxChild = box.firstElementChild;
+    const value = boxChild.getAttribute("value")
+
     if (boxChild.textContent) {
         return;
     }
@@ -12,7 +39,8 @@ function addLetter(box) {
     if (turn) {
         boxChild.textContent = "x";
         boxChild.className = "x";
-        turnDisplay.textContent = "O"
+        turnDisplay.textContent = "O";
+        board[value] = "x"
         turn = false;
         return;
     }
@@ -21,11 +49,16 @@ function addLetter(box) {
         boxChild.textContent = "o";
         boxChild.className = "o";
         turnDisplay.textContent = "X"
+        board[value] = "o";
         turn = true;
         return;
     }
 };
 
+// Box event listener
 boxes.forEach((box) => {
-    box.addEventListener("click", () => {addLetter(box)})
+    box.addEventListener("click", () => {
+        addLetter(box);
+        checkWinner();
+    })
 });
