a9be23b03a523c31e0c37287f0eeed3dc61555b5
Alexander Hartigan <alexandermhartigan@gmail.com>
2025-03-01 20:38:51 -0800

    added text functionality

--- public/js/play.js
+++ public/js/play.js
@@ -1,1 +1,31 @@
-console.log("hello")
+const boxes = document.querySelectorAll(".box");
+const turnDisplay = document.getElementById("turn-display")
+
+let turn = true;
+
+function addLetter(box) {
+    const boxChild = box.firstElementChild;
+    if (boxChild.textContent) {
+        return;
+    }
+
+    if (turn) {
+        boxChild.textContent = "x";
+        boxChild.className = "x";
+        turnDisplay.textContent = "O"
+        turn = false;
+        return;
+    }
+
+    if (!turn) {
+        boxChild.textContent = "o";
+        boxChild.className = "o";
+        turnDisplay.textContent = "X"
+        turn = true;
+        return;
+    }
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {addLetter(box)})
+});
